1abcf49d12f0de65db0d1c8ccfacdf16bc60813e
Kevin <kevin@rocher-dev.com>
2022-04-30 21:30:54 +0200

    v 0.1.1
    - Check if dependencies are installed.

--- init.sh
+++ init.sh
@@ -9,10 +9,29 @@ SCRIPTDIR="$( dirname "${BASH_SOURCE[0]}" )/";
 cat <<EOF
 
 ###################################
-## DK Site Checker v 0.1.0
+## DK Site Checker v 0.1.1
 ###################################
 
 EOF
+
+###################################
+## Check installed
+###################################
+
+_main_commands="curl node";
+for i in $_main_commands
+do
+    command -v "$i" >/dev/null 2>&1 || { echo >&2 "You need to install \"${i}\" to continue."; return 0; }
+done;
+
+_main_node_packages="puppeteer html-validator-cli";
+for package in $_main_node_packages
+do
+    if [ `npm list -g | grep -c $package` -eq 0 ]; then
+        echo  "You need to install the node package \"${package}\" to continue.";
+        return 0;
+    fi
+done;
 
 ###################################
 ## Load helpers
