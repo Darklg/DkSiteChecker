1822878bfaa84950180f2f6825010ad2cb8122d0
Kevin <kevin@rocher-dev.com>
2022-05-01 22:42:01 +0200

    v 0.2.0
    - Install requirements.
    - Load url list as an argument.
    - Updated Readme.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DkSiteChecker
-A tool to quickly check if a website has errors
+
+A tool to quickly check if a website has errors.
+
+## How to
+
+Call the script with the filename of a list of URLs to check in parameter.
+
+Example :
+
+```sh
+. ~/DkSiteChecker/init.sh url-list.txt;
+```
+
+## Required tools
+
+- cURL.
+- Node / npm.
--- init.sh
+++ init.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# npm i -g html-validator-cli
-# npm i -g puppeteer
-# Curl
-
 SCRIPTDIR="$( dirname "${BASH_SOURCE[0]}" )/";
 
 cat <<EOF
 
 ###################################
-## DK Site Checker v 0.1.1
+## DK Site Checker v 0.2.0
 ###################################
 
 EOF
@@ -18,7 +14,7 @@ EOF
 ## Check installed
 ###################################
 
-_main_commands="curl node";
+_main_commands="curl node npm";
 for i in $_main_commands
 do
     command -v "$i" >/dev/null 2>&1 || { echo >&2 "You need to install \"${i}\" to continue."; return 0; }
@@ -28,8 +24,8 @@ _main_node_packages="puppeteer html-validator-cli";
 for package in $_main_node_packages
 do
     if [ `npm list -g | grep -c $package` -eq 0 ]; then
-        echo  "You need to install the node package \"${package}\" to continue.";
-        return 0;
+        echo  "Installing the node package \"${package}\" to continue.";
+        npm i -g "${package}";
     fi
 done;
 
@@ -38,6 +34,21 @@ done;
 ###################################
 
 . "${SCRIPTDIR}inc/helpers.sh";
+
+###################################
+## Load URL list
+###################################
+
+_DKSITEURLLIST="";
+if [[ -f "wputools-urls.txt" ]];then
+    _DKSITEURLLIST="wputools-urls.txt";
+fi;
+if [[ -f "../wputools-urls.txt" ]];then
+    _DKSITEURLLIST="../wputools-urls.txt";
+fi;
+if [[ "${1}" != "" && -f "${1}" ]];then
+    _DKSITEURLLIST="${1}";
+fi;
 
 ###################################
 ## Run urls
@@ -50,12 +61,8 @@ while read line; do
     echo "";
     echo "####################";
     echo "## Test : ${line}";
-    echo "####################";
     echo "";
     dksitechecker_html_validator "${line}";
     dksitechecker_docspeed "${line}";
     dksitechecker_jscheck "${line}";
-done < "wputools-urls.txt"
-
-
-
+done < "${_DKSITEURLLIST}";
